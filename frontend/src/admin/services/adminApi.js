bafee2ccf11c973f2a7b82206f46070876a0da14
manishaa15-07 <manishachoudhary244365@gmail.com>
2025-08-15 18:40:41 +0530

    some changes

--- frontend/src/admin/services/adminApi.js
+++ frontend/src/admin/services/adminApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'https://poonamattiree-kffkoqu0d-manisha-s-projects-f984aff4.vercel.app/api';
+const API_URL = 'https://poonamattiree.vercel.app';
 
 // Create axios instance with default config
 const adminAPI = axios.create({
